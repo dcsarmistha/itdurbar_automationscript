ac026901ccc4b08fc815ba8c4f5ba8f13e49d4d2
dcsarmistha <sarmisthadc2@gmail.com>
2026-06-20 06:28:22 +0545

    added github actions ci pipeline

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,18 +2,30 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import pages.LoginPage;
 
 public class BaseTest {
+
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // 👇 detect GitHub Actions (CI environment)
+        boolean isCI = System.getenv("CI") != null;
+
+        if (isCI) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
@@ -23,6 +35,7 @@ public class BaseTest {
             driver.quit();
         }
     }
+
     public WebDriver getDriver() {
         return driver;
     }
